5f4648075f35bebf7feea29de740aa9c2b8df352
Eluagu Franklin <167933352+Biggysmallzprod@users.noreply.github.com>
2024-07-23 18:45:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const navToggle = document.createElement("div");
-  navToggle.className = "nav-toggle";
-  navToggle.innerHTML =
-    '<i class="fas fa-bars"></i><i class="fas fa-times"></i>';
-  document.querySelector("header .container").appendChild(navToggle);
+document.addEventListener('DOMContentLoaded', function () {
+  const navToggle = document.getElementById('nav-toggle');
+  const navUl = document.getElementById('nav-ul');
 
-  navToggle.addEventListener("click", function () {
-    document.querySelector("nav ul").classList.toggle("showing");
-    navToggle.classList.toggle("active");
+  navToggle.addEventListener('click', function () {
+    navUl.classList.toggle('showing');
+    navToggle.classList.toggle('active');
   });
 });
